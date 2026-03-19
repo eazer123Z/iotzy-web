190d9c01e2d789d699752e576c81afea7e3e8694
eazer123Z <rendyaulianur@gmail.com>
2026-03-19 08:04:39 +0800

    Disable SSL certificate verification for MySQL

--- config/database.php
+++ config/database.php
@@ -41,6 +41,7 @@ function getLocalDB(): ?PDO {
         $pdo = new PDO($dsn, $u, $p, [
             PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
             PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
         ]);
         $pdo->exec("SET NAMES " . DB_CHARSET);
     } catch (PDOException $e) {
